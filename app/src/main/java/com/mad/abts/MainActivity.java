6748ec38a5e0d30ae42dad57c0e33a2992be7524
ThevinRahu <thevinvr@gmail.com>
2021-08-10 22:19:13 +0530

    cleared

--- app/src/main/java/com/mad/abts/MainActivity.java
+++ app/src/main/java/com/mad/abts/MainActivity.java
@@ -11,9 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-/*branch thevin */
-    /*commented to test*/
 
-
-    public void Ok{}
 }
