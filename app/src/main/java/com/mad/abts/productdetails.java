0c8ec0e7069cfadab7f5e32a58d9b5d558e3780b
Buddhima <it20071998@my.sliit.lk>
2021-08-14 21:08:30 +0530

    product details done

---
+++ app/src/main/java/com/mad/abts/productdetails.java
@@ -0,0 +1,14 @@
+package com.mad.abts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class productdetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productdetails);
+    }
+}
