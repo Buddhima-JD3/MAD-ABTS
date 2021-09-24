a1ceee7ef6aa4ccf1aac3f5e238de7aa688d78a1
SenaraM <senaramallikahewa@gmail.com>
2021-09-25 00:58:41 +0530

    Teams6(Statistics) insert and read

---
+++ app/src/main/java/com/mad/abts/database/TeamStatistics.java
@@ -0,0 +1,21 @@
+package com.mad.abts.database;
+
+import android.provider.BaseColumns;
+
+public class TeamStatistics {
+
+    public TeamStatistics(){
+
+    }
+
+    public static class TeamStat implements BaseColumns{
+        public static final String TABLE_NAME = "teamStatistics";
+        public static final String COLUMN_NAME_TEAMLOGO = "teamlogo";
+        public static final String COLUMN_NAME_TEAMNAME = "teamname";
+        public static final String COLUMN_NAME_HIGHESTTOTAL = "highesttotal";
+        public static final String COLUMN_NAME_LOWESTTOTAL = "lowesttotal";
+        public static final String COLUMN_NAME_MOSTRUNS = "mostruns";
+        public static final String COLUMN_NAME_MOSTWICKETS = "mostwickets";
+        public static final String COLUMN_NAME_BOWLERNAME = "bowlername";
+    }
+}
--- app/src/main/java/com/mad/abts/teams6.java
+++ app/src/main/java/com/mad/abts/teams6.java
@@ -1,11 +1,10 @@
 package com.mad.abts;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
@@ -21,48 +20,53 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.mad.abts.database.DBHelperSenara;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
-import com.mad.abts.database.DBHelperSenara;
+import com.mad.abts.database.DBHelper;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-public class teams4  extends AppCompatActivity{
-    public final static int PICK_IMAGE_REQUEST = 999;
-    private Uri imageFile1;
+public class teams6 extends AppCompatActivity{
+    public final static int PICK_IMAGE_REQUEST=999;
+    private Uri imageFilePath1;
     Bitmap img1;
-    EditText team;
-    String mteam;
-    ImageView logo;
+    EditText teamname, bowlername, highesttotal, lowesttotal, mostruns, mostwickets;
+    String mteamname, mbowlername;
+    int mhighesttotal, mlowesttotal, mmostruns, mmostwickets;
+    ImageView teamlogo;
     private Context context;
     private String[] cameraPermission;
     private String[] storagePermission;
 
     @SuppressLint("WrongViewCast")
     @Override
-    protected  void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teams4);
+        setContentView(R.layout.activity_adminmatches);
         context=this;
-        logo = findViewById(R.id.logoimage1);
-        team = findViewById(R.id.tname);
-
+        teamlogo = findViewById(R.id.logoinsertname2);
+        teamname= findViewById(R.id.inteam2);
+        highesttotal = findViewById(R.id.insert1);
+        lowesttotal = findViewById(R.id.insert2);
+        mostruns = findViewById(R.id.insert3);
+        mostwickets = findViewById(R.id.insert4);
+        bowlername = findViewById(R.id.insert5);
         cameraPermission =new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
         storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
-        logo.setOnClickListener(new View.OnClickListener() {
+        teamlogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ActivityCompat.requestPermissions(
-                        teams4.this,
+                        teams6.this,
                         new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                         PICK_IMAGE_REQUEST
                 );
             }
-
         });
-
 
     }
 
@@ -79,14 +83,15 @@ public class teams4  extends AppCompatActivity{
         if(requestCode==PICK_IMAGE_REQUEST){
             if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
                 //gallery intent
-                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                galleryIntent.setType("image/*");
-                startActivityForResult(galleryIntent,PICK_IMAGE_REQUEST);
+                Intent gallertIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                gallertIntent.setType("image/*");
+                startActivityForResult(gallertIntent,PICK_IMAGE_REQUEST);
             }else{
             }
             return;
         }
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -103,7 +108,7 @@ public class teams4  extends AppCompatActivity{
             if(resultCode ==RESULT_OK){
                 Uri resultUri = result.getUri();
                 //set image choose from gallery to image view
-                logo.setImageURI(resultUri);
+                teamlogo.setImageURI(resultUri);
 
             }else if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
                 Exception error = result.getError();
@@ -113,34 +118,34 @@ public class teams4  extends AppCompatActivity{
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    public void saveTeams(View view){
-        byte[] images1 = imageViewToByte(logo);
-        mteam = team.getText().toString();
-
+    public void saveTeamStat(View view){
+        byte[] image = imageViewToByte(teamlogo);
+        mteamname = teamname.getText().toString();
+        mhighesttotal = Integer.parseInt(highesttotal.getText().toString());
+        mlowesttotal = Integer.parseInt(lowesttotal.getText().toString());
+        mmostruns = Integer.parseInt(mostruns.getText().toString());
+        mmostwickets = Integer.parseInt(mostwickets.getText().toString());
+        mbowlername = bowlername.getText().toString();
 
         DBHelperSenara dbHelper = new DBHelperSenara(this);
-        if(mteam.isEmpty()){
-            Toast.makeText(this, "Enter Teams", Toast.LENGTH_SHORT).show();
+        if(mteamname.isEmpty()){
+            Toast.makeText(this, "Enter Team", Toast.LENGTH_SHORT).show();
         }else{
-            Long inserted = dbHelper.addTeams(images1,mteam);
+            dbHelper.addTeamStat(image, mteamname, mhighesttotal, mlowesttotal, mmostruns, mmostwickets, mbowlername);
         }
+
     }
 
-    public void teams6(View view) {
-        Intent intent = new Intent(this, teams6.class);
-        startActivity(intent);
-    }
-
-    public void teams4(View view) {
-        Intent intent = new Intent(this, teams4.class);
-        startActivity(intent);
-    }
     public void admin(View view) {
         Intent intent = new Intent(this, adminhome.class);
         startActivity(intent);
     }
     public void adminsidebar(View view) {
         Intent intent = new Intent(this, adminsidebar.class);
+        startActivity(intent);
+    }
+    public void teams6(View view) {
+        Intent intent = new Intent(this, teams6.class);
         startActivity(intent);
     }
 }
